62104f506a5fceb99c4b0269bd6b9bd8c650b829
linh <linhnvnde160328@fpt.edu.vn>
2024-07-17 15:46:34 +0700

    manage job post

---
+++ src/models/UserModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; // Import Schema from mongoose
+
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema(
+  {
+    name: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    address: { type: String },
+    phone: { type: Number },
+    access_token: { type: String },
+    refresh_token: { type: String },
+    role: { type: String },
+    candidateProfile: { type: Schema.Types.ObjectId, ref: "CandidateProfile" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
